5f43ba748aee8dbc19405a1753a39abf96286cdc
Adekanye Oluwatosin Daniel <mydharn@gmail.com>
2026-06-16 15:38:12 +0100

    Introduce a controller to get the best staff to generate better reveneue

--- Smart_Inventory_sales_management/Backend/Routes/reportRoute.js
+++ Smart_Inventory_sales_management/Backend/Routes/reportRoute.js
@@ -1,12 +1,13 @@
 const express = require('express')
 const reportRoute = express.Router()
 
-const {getBestSellingProduct, getReport} = require('../controllers/reportController')
+const {getBestSellingProduct, getReport, getBestStaff} = require('../controllers/reportController')
 const authMiddleware = require('../Middlewares/authMiddleware')
 const userManagementMiddleware = require('../Middlewares/userManagementMiddleware')
 
-reportRoute.route('/:date').get(authMiddleware, userManagementMiddleware, getReport)
 reportRoute.route('/best-product').get(authMiddleware, userManagementMiddleware, getBestSellingProduct)
+reportRoute.route('/best-staff').get(authMiddleware, userManagementMiddleware, getBestStaff)
+reportRoute.route('/:date').get(authMiddleware, userManagementMiddleware, getReport)
 
 
 module.exports = reportRoute

22c6bb65a7e5ac2ceebac5a7a0171c826eec966f
Adekanye Oluwatosin Daniel <mydharn@gmail.com>
2026-07-07 13:59:18 +0100

    Initiating withrawal from my wallet to Bank Account Using PAYSTACK

--- Digital_Wallet_and_Payment_Processing_System/Backend/controllers/withdrawalController.js
+++ Digital_Wallet_and_Payment_Processing_System/Backend/controllers/withdrawalController.js
@@ -1,38 +1,87 @@
 const mongoose = require('mongoose')
 const generateReference = require('../utils/generateReference')
 const {getBankCode, verifyReference, initiateWithdrawal} = require('../utils/payStackService')
+const bcrypt = require('bcrypt')
 
 const walletModel = require('../Models/Wallet')
 const transactionModel = require('../Models/Transaction')
 
 const withdrawFunds = async(req, res)=>{
 
-    const session = await mongoose.session()
+    const session = await mongoose.startSession()
     session.startTransaction();
     try{
         //user Wallet
-        const {walletNumber, pin, bankAccount, bankName, amount} = req.body;
+        let {pin, bankAccount, bankName, amount} = req.body;
         
         if(!walletNumber || !pin || !bankAccount || !bankName || !amount){
             session.abortTransaction()
             return res.status(400).json({
                 status: "failed",
-                message: ""
+                message: "Empty fields"
             })
         }
         //Validate wallet Number
-        const wallet = await walletModel.findOne({walletNumber: walletNumber}).select("+pin")
+        const wallet = await walletModel.findOne({userId: req.user._id}).select("+pin")
+        if(!wallet){
+            session.abortTransaction()
+            return res.status(404).json({
+                status: "failed",
+                message: "Wallet could not be found"
+            })
+        }
         //Request Withrawal
 
-        //Validate Pin
+        const referenceId = generateReference();
 
+        //Validate Pin
+        if(typeof pin === 'number'){
+            pin = String(pin)
+        }
+
+        //Compare pin
+        const isPin = await bcrypt.compare(pin, wallet.pin)
+        if(!isPin){
+            session.abortTransaction()
+            return res.status(400).json({
+                status: 'failed',
+                message: 'pin in Invalid'
+            })
+        }
         //validate Balance
+        if(wallet.amount < Number(amount)){
+            session.abortTransaction()
+            return res.status(400).json({
+                status:"failed",
+                message: "Insufficient balance"
+            })
+        }
 
         //validate bank Account
+        const validateBank = await getBankCode(bankName)
 
+        if(!validateBank){
+            session.abortTransaction()
+            return res.status(404).json({
+                status: "failed",
+                message: "Invalid bank name or Account"
+            })
+        }
         //Create transfer recipient
+        const transaction = new transactionModel({
+            walletId: wallet._id,
+            userId: req.user._id,
+            type_of_transaction: "WITHRDRAWAL",
+            status: "PENDING",
+            referenceId: referenceId,
+            amount: String(amount)
+        })
 
+        await transaction.save({session});
+
+        
         //initiate transfer
+        const initiatePaystackWithdrawal = await initiateWithdrawal(Number(amount),recipient, referenceId, "undefined")
 
         //process tranfere(PAYSTACk)
 
@@ -49,4 +98,106 @@ const withdrawFunds = async(req, res)=>{
         })
     }
 }
+
+
+const verificationController = async(req, res)=>{
+    const session = await mongoose.startSession()
+    session.startTransaction()
+     try {
+       const { reference } = req.params;
+   
+       const transaction = await transactionModel
+         .findOne({ referenceId: reference })
+         .session(session);
+       const wallet = await walletModel
+         .findByOne({ userId: req.user._id })
+         .session(session);
+   
+       if (!transaction || !wallet) {
+         await session.abortTransaction();
+         return res.status(404).json({
+           status: "failed",
+           message: "Cannot find wallet or transaction",
+         });
+       }
+   
+       if (transaction.status === "SUCCESS") {
+         await session.abortTransaction();
+         return res.status(404).json({
+           status: "failed",
+           message: "This transaction is already processed",
+         });
+       }
+   
+       const initialWalletBalance = wallet.balance;
+   
+       const verification = await verifyReference(transaction.referenceId);
+   
+       //Check the integrity of the transaction using reference ID
+       const payment = verification.data;
+       if (payment.reference !== transaction.referenceId) {
+         await session.abortTransaction();
+         return res.status(400).json({
+           status: "failed",
+           message: "ReferenceId does not match",
+         });
+       }
+   
+       // Verify that the amount is the same
+   
+       if (payment.amount !== transaction.amount * 100) {
+         await session.abortTransaction();
+         return res.status(400).json({
+           status: "failed",
+           message: "amount processed is not Valid",
+         });
+       }
+   
+   
+       //Check for currency Integrity
+       if(payment.currency !== wallet.currency){
+           await session.abortTransaction()
+           return res.status(400).json({
+               status: "failed",
+               message: "currency mismatch"
+           })
+       }
+   
+       //perform transaction and update the status of the transaction
+   
+       if (verification.data.status === "success") {
+         wallet.balance = Number(transaction.amount) + initialWalletBalance;
+         transaction.status = "SUCCESS";
+         await transaction.save({ session });
+         await wallet.save({ session });
+         await session.commitTransaction();
+   
+         return res.status(200).json({
+           status: "success",
+           message: `You have been credited with ${transaction.amount}`,
+           balance: wallet.balance,
+           transaction
+         });
+       }
+   
+       transaction.status = "FAILED";
+       await transaction.save({ session });
+       await session.commitTransaction();
+   
+       return res.status(400).json({
+         status: "failed",
+         message: "The transaction failed",
+       });
+     } catch (e) {
+       session.abortTransaction();
+       console.error(e);
+       res.status(500).json({
+         status: "failed",
+         message: "verification could not be completed",
+       });
+     } finally {
+       session.endSession();
+     }
+   };
+   
 module.exports = {withdrawFunds}
--- Digital_Wallet_and_Payment_Processing_System/Backend/utils/payStackService.js
+++ Digital_Wallet_and_Payment_Processing_System/Backend/utils/payStackService.js
@@ -1,99 +1,145 @@
-const axios  = require('axios')
-const PAYSTACK_SECRET_KEY = process.env.PAYSTACK_SECRET_KEY
+const axios = require("axios");
+const PAYSTACK_SECRET_KEY = process.env.PAYSTACK_SECRET_KEY;
 
 /**
- * INITIATION OF TRANSACTION IN ORDER TO FUND WALLET 
+ * INITIATION OF TRANSACTION IN ORDER TO FUND WALLET
  */
-const initiateTransaction = async(email, amount, reference)=>{
-    try{
-        const response = await axios.post("https://api.paystack.co/transaction/initialize", {
-            email,
-            amount: amount * 100,
-            reference,
-            callback_url
-        }, {
-            headers:{
-                Authorization: `Bearers ${PAYSTACK_SECRET_KEY}`,
-                "Content-Type": 'application/json'
-            }
-        })
+const initiateTransaction = async (email, amount, reference) => {
+  try {
+    const response = await axios.post(
+      "https://api.paystack.co/transaction/initialize",
+      {
+        email,
+        amount: amount * 100,
+        reference,
+        callback_url,
+      },
+      {
+        headers: {
+          Authorization: `Bearers ${PAYSTACK_SECRET_KEY}`,
+          "Content-Type": "application/json",
+        },
+      },
+    );
 
-        return response.data
-    }
-    catch(e){
-        throw new Error(`This is error is coming from paystack , ${e}`)
-    }
-}
-
-/**
- *  GET BANK CODE FROM THE NAME PASSED TO REQEST.BODY 
- */
-
-const getBankCode = async(bankName)=>{
-    try{
-        const response = await axios.get('https://api.paystack.co/bank', {
-            headers:{
-                Authorization: `Bearers ${PAYSTACK_SECRET_KEY}`,
-                "Content-Type": 'application/json'
-            }
-
-        })
-        const banks = await response.data.data
-        const bank = banks.find((b)=> b.name.toLowerCase() ===  bankName.toLowerCase())
-
-        if(!bank) throw new Error("Bank Name is not found")
-
-        return bank.code
-
-    }catch(e){
-        throw new Error(`This is error from getBank , ${e}`)
-    }
-}
+    return response.data;
+  } catch (e) {
+    throw new Error(`This is error is coming from paystack , ${e}`);
+  }
+};
 
 /**
- *INITIATE WITHDRAWAL 
+ *  GET BANK CODE FROM THE NAME PASSED TO REQEST.BODY
  */
 
+const getBankCode = async (bankName) => {
+  try {
+    const response = await axios.get("https://api.paystack.co/bank", {
+      headers: {
+        Authorization: `Bearers ${PAYSTACK_SECRET_KEY}`,
+        "Content-Type": "application/json",
+      },
+    });
+    const banks = await response.data.data;
+    const bank = banks.find(
+      (b) => b.name.toLowerCase() === bankName.toLowerCase(),
+    );
 
- const initiateWithdrawal = async(source, amount, recipient, reference, reason)=>{
-    try{
-        const response = await axios.post("https://api.paystack.co/transfer",{
-            "source": source,
-            "amount": amount,
-            "recipient": recipient,
-            "reference": reference,
-            "reason": reason
-        }, {
-            headers: {
-                Authorization: `Bearers ${PAYSTACK_SECRET_KEY}`, 
-                "Content-type": 'application.json'
-            }
-        })
+    if (!bank) throw new Error("Bank Name is not found");
 
+    return bank.code;
+  } catch (e) {
+    throw new Error(`This is error from getBank , ${e}`);
+  }
+};
 
-
-    }catch(e){
-        throw new Error(`Error from Withdrawal process, ${e}`)
-    }
- }
 /**
- * 
+ *INITIATE WITHDRAWAL
+ */
+
+const initiateWithdrawal = async (
+  amount,
+  recipient,
+  reference,
+  reason,
+) => {
+  try {
+    const response = await axios.post(
+      "https://api.paystack.co/transfer",
+      {
+        source: "balance",
+        amount: amount * 100,
+        recipient: recipient,
+        reference: reference,
+        reason: reason,
+      },
+      {
+        headers: {
+          Authorization: `Bearer ${PAYSTACK_SECRET_KEY}`,
+          "Content-type": "application.json",
+        },
+      },
+    );
+
+    return response.data;
+
+    if (!response.ok) throw new Error("data could not be fetched");
+  } catch (e) {
+    throw new Error(`Error from Withdrawal process, ${e}`);
+  }
+};
+/**
+ *
  * VERIFY REFERENCE
  */
-const verifyReference = async(reference)=> {
+const verifyReference = async (reference) => {
+  try {
+    const response = await axios.get(
+      `https://api.paystack.co/transaction/verify/${reference}`,
+      {
+        headers: {
+          Authorization: `Bearers ${PAYSTACK_SECRET_KEY}`,
+          "Content-Type": "application/json",
+        },
+      },
+    );
+
+    return response.data;
+  } catch (e) {
+    throw new Error(`This is error from my verification, ${e}`);
+  }
+};
+
+/**
+ * CREATE RECIPIENT/BENEFICIARY
+ */
+
+const initiateRecipient = async(type, name, accountNumber, bankCode, currency)=>{
     try{
-        const response = await axios.get(`https://api.paystack.co/transaction/verify/${reference}`,{
-            headers: {
-                Authorization: `Bearers ${PAYSTACK_SECRET_KEY}`,
-                "Content-Type": 'application/json'
+        const response = await axios.post("https://api.paystack.co/transferrecipient", {
+            type: "nuban",
+            name: name,
+            account_Number: accountNumber,
+            bank_code: bankCode,
+            currency: "NGN"
+        },{
+            headers:{
+                Authorization: `Bearer ${PAYSTACK_SECRET_KEY}`,
+                "Content-Type": "application/json"
             }
         })
+        if(!response.ok) throw new Error("initiateRecipient just Failed")
 
-        return response.data
+        return response.data.recipient_code
     }catch(e){
-        throw new Error(`This is error from my verification, ${e}`)
+        throw new Error(`This is error from initiateRecipient`, e)
     }
 }
 
-
-module.exports = {initiateTransaction, verifyReference, initiateWithdrawal, getBankCode}
+module.exports = {
+  initiateTransaction,
+  verifyReference,
+  initiateWithdrawal,
+  getBankCode,
+  initiateRecipient
+};

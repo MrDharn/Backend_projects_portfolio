db53844d44dd8afc8ebad8de2e5d20b9a4729f35
Adekanye Oluwatosin Daniel <mydharn@gmail.com>
2026-08-14 03:44:02 +0100

    Improvement on my Portfolio API: BY using RBAC

---
+++ Dharn_Portfolio/backend/middlewares/AdminAuth.js
@@ -0,0 +1,38 @@
+const jwt = require("jsonwebtoken")
+
+const adminAuth = async(req, res, next)=>{
+    try{
+        const authHeader = req.get("authorization")
+        const token = authHeader && authHeader.split(" ")[1]
+        if(!token) {
+            return res.status(403).json({
+                status: "Failed",
+                message: "You are not authorized"
+            })
+        }
+
+        // IF TOKEN IS VALIDATED, THEN DECODE
+       const decodedToken = jwt.verify(token, process.env.JWT_SECRET_KEY)
+        if(!decodedToken.isAdmin){
+            return res.status(403).json({
+                status: false,
+                message: "ACcess Denied"
+            })
+        }
+
+        req.admin = decodedToken
+        return next()
+    }catch(e){
+        return res.status(401).json({
+            status: false,
+            message: "Invalid or token expired "
+        })
+    }
+
+    return res.status(401).json({
+        status: false,
+        message: "No token Provided"
+    })
+}
+
+module.exports = adminAuth

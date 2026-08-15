3412b668b6fa5c2660f782ec2b2cafcd109f08de
Adekanye Oluwatosin Daniel <mydharn@gmail.com>
2026-08-15 04:20:22 +0100

    Updated the project Route

--- Dharn_Portfolio/backend/routes/projectRoute.js
+++ Dharn_Portfolio/backend/routes/projectRoute.js
@@ -1,8 +1,18 @@
 const express = require('express')
 const projectRoute = express.Router();
-const {fetchProjects, fetchFeaturedProjects} = require('../controllers/projectController')
+const {fetchProjects, fetchFeaturedProjects, addProject, removeProject, editProject, trackProjectDownload} = require('../controllers/projectController')
+
+const adminAuth = require("../middlewares/AdminAuth")
 
 projectRoute.route('/').get(fetchProjects)
 projectRoute.route('/feature').get(fetchFeaturedProjects)
+projectRoute.route("/:id/download").get(trackProjectDownload)
 
+//FOR ADMIN
+
+projectRoute.route("/projects").post(adminAuth, addProject)
+
+projectRoute.route("/projects/:id").patch(adminAuth, editProject)
+
+projectRoute.route("/projects/:id").delete(adminAuth, removeProject)
 module.exports = projectRoute
